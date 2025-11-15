f54874bbce93f2f3b754db93e3ce61ea99d1a40b
Mohammad-Shayan-Asim <27100097@lums.edu.pk>
2025-11-15 12:52:01 +0500

    fedsam debug #10

--- fed_lib/fed_methods.py
+++ fed_lib/fed_methods.py
@@ -291,7 +291,7 @@ class FedSAM(FedMethod):
             aggregated_params = [torch.sum(torch.stack(param_group), dim=0) 
                                 for param_group in param_groups]
             
-            if len(local_models == 1):
+            if len(len(local_models) == 1):
                 aggregated_params = list(local_models[0].parameters())
             
             for aggregated_param, global_param in zip(aggregated_params, global_model.parameters()):
